38cef04364f7938c39b90b6f1d70d3da45fe5053
MaggyMage <mawubr@gmail.com>
2023-02-22 14:52:23 +0100

    Button random 2

--- MultiD-Minesweeper/Form1.cs
+++ MultiD-Minesweeper/Form1.cs
@@ -12,32 +12,32 @@ namespace MultiD_Minesweeper
 {
     public partial class Form1 : Form
     {
-        int[] order = { 0, 1, 2, 3 };
-        Random random = new Random();
-        string[] buttonPair = { "yes",  "no" ,  "no" ,  "no"  };
+        int[] orderX = { 0, 1, 2, 3 };
+        int[] orderY = { 0, 1, 2, 3 };
+        Random randomX = new Random(Guid.NewGuid().GetHashCode());
+        Random randomY = new Random(Guid.NewGuid().GetHashCode());
+        string[,] buttonPair = { { "yes", "no", "no", "no" } , { "no", "no", "no", "no" } , { "no", "no", "no", "no" } , { "no", "no", "no", "no" } };
 
         int count = 1;
-        int X = 4;
-        int Y = 1;
+        int XMax = 4;
         int YMax = 4;
 
         public Form1()
         {
             InitializeComponent();
-            order = order.OrderBy(x => random.Next()).ToArray();
+            orderX = orderX.OrderBy(x => randomX.Next()).ToArray();
+            orderY = orderY.OrderBy(x => randomY.Next()).ToArray();
 
-            for (int i = 1; i <= X; i++)
+            for (int j = 0; j < YMax; j++)
             {
-                Button b = new Button();
-                b.Text = buttonPair[order[i - 1]];
-                b.Name = count.ToString();
-                b.Size = new Size(35, 35);
-                b.Location = new Point(40 * (i + 1), 40 * Y);
-                Controls.Add(b);
-                if (i == X && Y < YMax)
+                for (int i = 0; i < XMax; i++)
                 {
-                    i = 0;
-                    Y++;
+                    Button b = new Button();
+                    b.Text = buttonPair[orderX[i], orderY[j]];
+                    b.Name = count.ToString();
+                    b.Size = new Size(35, 35);
+                    b.Location = new Point(40 * (i + 2), 40 * j);
+                    Controls.Add(b);
                 }
             }
 
