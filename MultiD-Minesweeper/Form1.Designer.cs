f98563bfb04d2fbe5860139813a6471da25a6851
MaggyMage <mawubr@gmail.com>
2023-03-13 16:21:30 +0100

    Made user input and different tab pages

--- MultiD-Minesweeper/Form1.Designer.cs
+++ MultiD-Minesweeper/Form1.Designer.cs
@@ -29,27 +29,216 @@
         private void InitializeComponent()
         {
             this.buttonStart = new System.Windows.Forms.Button();
+            this.comboBoxGameMode = new System.Windows.Forms.ComboBox();
+            this.labelGameMode = new System.Windows.Forms.Label();
+            this.labelDimensions = new System.Windows.Forms.Label();
+            this.labelLength = new System.Windows.Forms.Label();
+            this.labelMines = new System.Windows.Forms.Label();
+            this.textBoxDimensions = new System.Windows.Forms.TextBox();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxMines = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPagePlay = new System.Windows.Forms.TabPage();
+            this.tabPageMenu = new System.Windows.Forms.TabPage();
+            this.tabPageSettings = new System.Windows.Forms.TabPage();
+            this.tabPageGame = new System.Windows.Forms.TabPage();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.labelSettings = new System.Windows.Forms.Label();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPagePlay.SuspendLayout();
+            this.tabPageMenu.SuspendLayout();
+            this.tabPageSettings.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonStart
             // 
             this.buttonStart.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonStart.Location = new System.Drawing.Point(200, 141);
+            this.buttonStart.Location = new System.Drawing.Point(217, 267);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(367, 161);
+            this.buttonStart.Size = new System.Drawing.Size(311, 115);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "START";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
+            // comboBoxGameMode
+            // 
+            this.comboBoxGameMode.FormattingEnabled = true;
+            this.comboBoxGameMode.Location = new System.Drawing.Point(217, 233);
+            this.comboBoxGameMode.Name = "comboBoxGameMode";
+            this.comboBoxGameMode.Size = new System.Drawing.Size(311, 28);
+            this.comboBoxGameMode.TabIndex = 1;
+            // 
+            // labelGameMode
+            // 
+            this.labelGameMode.AutoSize = true;
+            this.labelGameMode.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGameMode.Location = new System.Drawing.Point(302, 201);
+            this.labelGameMode.Name = "labelGameMode";
+            this.labelGameMode.Size = new System.Drawing.Size(140, 29);
+            this.labelGameMode.TabIndex = 2;
+            this.labelGameMode.Text = "Gamemode";
+            // 
+            // labelDimensions
+            // 
+            this.labelDimensions.AutoSize = true;
+            this.labelDimensions.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDimensions.Location = new System.Drawing.Point(64, 73);
+            this.labelDimensions.Name = "labelDimensions";
+            this.labelDimensions.Size = new System.Drawing.Size(155, 29);
+            this.labelDimensions.TabIndex = 3;
+            this.labelDimensions.Text = "Dimensions";
+            // 
+            // labelLength
+            // 
+            this.labelLength.AutoSize = true;
+            this.labelLength.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLength.Location = new System.Drawing.Point(321, 73);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(103, 29);
+            this.labelLength.TabIndex = 4;
+            this.labelLength.Text = "Length";
+            // 
+            // labelMines
+            // 
+            this.labelMines.AutoSize = true;
+            this.labelMines.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMines.Location = new System.Drawing.Point(567, 73);
+            this.labelMines.Name = "labelMines";
+            this.labelMines.Size = new System.Drawing.Size(85, 29);
+            this.labelMines.TabIndex = 5;
+            this.labelMines.Text = "Mines";
+            // 
+            // textBoxDimensions
+            // 
+            this.textBoxDimensions.Location = new System.Drawing.Point(91, 102);
+            this.textBoxDimensions.Name = "textBoxDimensions";
+            this.textBoxDimensions.Size = new System.Drawing.Size(100, 26);
+            this.textBoxDimensions.TabIndex = 6;
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(322, 102);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(100, 26);
+            this.textBoxLength.TabIndex = 7;
+            // 
+            // textBoxMines
+            // 
+            this.textBoxMines.Location = new System.Drawing.Point(559, 102);
+            this.textBoxMines.Name = "textBoxMines";
+            this.textBoxMines.Size = new System.Drawing.Size(100, 26);
+            this.textBoxMines.TabIndex = 8;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageMenu);
+            this.tabControl1.Controls.Add(this.tabPagePlay);
+            this.tabControl1.Controls.Add(this.tabPageSettings);
+            this.tabControl1.Controls.Add(this.tabPageGame);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(785, 450);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPagePlay
+            // 
+            this.tabPagePlay.Controls.Add(this.buttonStart);
+            this.tabPagePlay.Controls.Add(this.comboBoxGameMode);
+            this.tabPagePlay.Controls.Add(this.labelGameMode);
+            this.tabPagePlay.Controls.Add(this.labelMines);
+            this.tabPagePlay.Controls.Add(this.textBoxMines);
+            this.tabPagePlay.Controls.Add(this.labelDimensions);
+            this.tabPagePlay.Controls.Add(this.textBoxLength);
+            this.tabPagePlay.Controls.Add(this.labelLength);
+            this.tabPagePlay.Controls.Add(this.textBoxDimensions);
+            this.tabPagePlay.Location = new System.Drawing.Point(4, 29);
+            this.tabPagePlay.Name = "tabPagePlay";
+            this.tabPagePlay.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePlay.Size = new System.Drawing.Size(777, 417);
+            this.tabPagePlay.TabIndex = 0;
+            this.tabPagePlay.Text = "Play";
+            this.tabPagePlay.UseVisualStyleBackColor = true;
+            // 
+            // tabPageMenu
+            // 
+            this.tabPageMenu.Controls.Add(this.buttonSettings);
+            this.tabPageMenu.Controls.Add(this.buttonPlay);
+            this.tabPageMenu.Location = new System.Drawing.Point(4, 29);
+            this.tabPageMenu.Name = "tabPageMenu";
+            this.tabPageMenu.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMenu.Size = new System.Drawing.Size(777, 417);
+            this.tabPageMenu.TabIndex = 1;
+            this.tabPageMenu.Text = "Menu";
+            this.tabPageMenu.UseVisualStyleBackColor = true;
+            // 
+            // tabPageSettings
+            // 
+            this.tabPageSettings.Controls.Add(this.labelSettings);
+            this.tabPageSettings.Location = new System.Drawing.Point(4, 29);
+            this.tabPageSettings.Name = "tabPageSettings";
+            this.tabPageSettings.Size = new System.Drawing.Size(777, 417);
+            this.tabPageSettings.TabIndex = 2;
+            this.tabPageSettings.Text = "Settings";
+            this.tabPageSettings.UseVisualStyleBackColor = true;
+            // 
+            // tabPageGame
+            // 
+            this.tabPageGame.Location = new System.Drawing.Point(4, 29);
+            this.tabPageGame.Name = "tabPageGame";
+            this.tabPageGame.Size = new System.Drawing.Size(777, 417);
+            this.tabPageGame.TabIndex = 3;
+            this.tabPageGame.Text = "Game";
+            this.tabPageGame.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlay.Location = new System.Drawing.Point(196, 73);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(346, 131);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // labelSettings
+            // 
+            this.labelSettings.AutoSize = true;
+            this.labelSettings.Font = new System.Drawing.Font("Stencil", 40F);
+            this.labelSettings.Location = new System.Drawing.Point(185, 14);
+            this.labelSettings.Name = "labelSettings";
+            this.labelSettings.Size = new System.Drawing.Size(409, 95);
+            this.labelSettings.TabIndex = 3;
+            this.labelSettings.Text = "Settings";
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Font = new System.Drawing.Font("Stencil", 30F);
+            this.buttonSettings.Location = new System.Drawing.Point(196, 229);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(346, 80);
+            this.buttonSettings.TabIndex = 1;
+            this.buttonSettings.Text = "Settings";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            this.buttonSettings.Click += new System.EventHandler(this.buttonSettings_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonStart);
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPagePlay.ResumeLayout(false);
+            this.tabPagePlay.PerformLayout();
+            this.tabPageMenu.ResumeLayout(false);
+            this.tabPageSettings.ResumeLayout(false);
+            this.tabPageSettings.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +246,22 @@
         #endregion
 
         private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ComboBox comboBoxGameMode;
+        private System.Windows.Forms.Label labelGameMode;
+        private System.Windows.Forms.Label labelDimensions;
+        private System.Windows.Forms.Label labelLength;
+        private System.Windows.Forms.Label labelMines;
+        private System.Windows.Forms.TextBox textBoxDimensions;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxMines;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageMenu;
+        private System.Windows.Forms.TabPage tabPagePlay;
+        private System.Windows.Forms.TabPage tabPageSettings;
+        private System.Windows.Forms.TabPage tabPageGame;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Label labelSettings;
     }
 }
 
--- MultiD-Minesweeper/Form1.cs
+++ MultiD-Minesweeper/Form1.cs
@@ -12,6 +12,10 @@ namespace MultiD_Minesweeper
 {
     public partial class Form1 : Form
     {
+        int dimensions;
+        int length;
+        int mines;
+
         int[] orderX = { 0, 1, 2, 3 };
         int[] orderY = { 0, 1, 2, 3 };
         Random randomX = new Random(Guid.NewGuid().GetHashCode());
@@ -27,26 +31,68 @@ namespace MultiD_Minesweeper
             InitializeComponent();
             orderX = orderX.OrderBy(x => randomX.Next()).ToArray();
             orderY = orderY.OrderBy(x => randomY.Next()).ToArray();
+            tabControl1.TabPages.Clear();
+            tabControl1.TabPages.Insert(0, tabPageMenu);
 
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            buttonStart.Visible = false; //Hide the start button
 
-            //generate the game board
-            for (int j = 0; j < YMax; j++)
+            bool errorNum = int.TryParse(textBoxDimensions.Text, out dimensions);
+            if (errorNum == true)
+                errorNum = int.TryParse(textBoxLength.Text, out length);
+            if (errorNum == true)
+                errorNum = int.TryParse(textBoxMines.Text, out mines);
+
+            bool errorMines = true;
+            if (mines >= Math.Pow(length, dimensions))
+                errorMines = false;
+                
+
+            if(errorNum == true && errorMines == true)
             {
-                for (int i = 0; i < XMax; i++)
+                tabControl1.TabPages.Clear();
+                tabControl1.TabPages.Insert(0, tabPageGame);
+
+                //generate the game board
+                for (int j = 0; j < YMax; j++)
                 {
-                    Button b = new Button();
-                    b.Text = buttonPair[orderX[i], orderY[j]];
-                    b.Name = count.ToString();
-                    b.Size = new Size(35, 35);
-                    b.Location = new Point(40 * (i + 2), 40 * j);
-                    Controls.Add(b);
+                    for (int i = 0; i < XMax; i++)
+                    {
+                        Button b = new Button();
+                        b.Text = buttonPair[orderX[i], orderY[j]];
+                        b.Name = count.ToString();
+                        b.Size = new Size(35, 35);
+                        b.Location = new Point(40 * (i + 2), 40 * j);
+                        Controls.Add(b);
+                        tabPageGame.Controls.Add(b);
+                    }
                 }
             }
+            else if(errorNum == false)
+            {
+                //Show error if it couldn't parse the numbers
+                MessageBox.Show("Error, try again with real numbers", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (errorMines == false)
+            {
+                //Show error if the number of mines exceeds the number of tiles
+                MessageBox.Show("Error, too many mines", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Clear();
+            tabControl1.TabPages.Insert(0, tabPagePlay);
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Clear();
+            tabControl1.TabPages.Insert(0, tabPageSettings);
         }
     }
 }
