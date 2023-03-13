1fb5f26ef6cf218da81fef95457a18f0e60bd186
MaggyMage <mawubr@gmail.com>
2023-03-13 14:14:33 +0100

    Create Core.cs
    Core made

---
+++ MultiD-Minesweeper/Core.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiD_Minesweeper
+{
+    internal class GameCore
+    {
+        public int[,] Game()
+        {
+            int[,] board = new int[,] { { 1, 1 }, { 2, 2 } };
+            //test
+            return board;
+
+        }
+    }
+}
